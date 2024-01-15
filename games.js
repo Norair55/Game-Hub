a1703a585bb81e2c65842f192b4aea36eb7eb5ad
Norair55 <norayr.jurnal.123@mail.ru>
2024-01-15 20:32:11 +0300

    initial commit

---
+++ games.js
@@ -0,0 +1,10 @@
+const menuBar=document.querySelector(".img-wrapper") 
+const listMobile=document.querySelector(".list-mobile")
+
+menuBar.addEventListener("keypress",()=>{   
+
+listMobile.classList.toggle("menu-open"); 
+
+
+})  
+
